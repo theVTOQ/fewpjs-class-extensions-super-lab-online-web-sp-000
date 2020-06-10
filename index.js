327797584ac8867d2bec69309302e1ce0256c6a2
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2020-06-10 15:58:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your classes here
+class Tree{
+  constructor(species){
+    this.species = species;
+  }
+
+  static definition(){
+    return "A tree is a perennial plant with an elongated stem, or trunk, supporting branches and leaves."
+  }
+}
+
+class Deciduous extends Tree{
+  constructor(species, name){
+    super(species);
+    this.name = name;
+  }
+}
