f17e8fdb215acbbaa1232c3783694b396dc735c4
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2020-06-10 15:59:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,4 +14,19 @@ class Deciduous extends Tree{
     super(species);
     this.name = name;
   }
+
+  static definition(){
+    return `${super(definition)} Deciduous trees shed their leaves annually.`;
+  }
+}
+
+class Evergreen extends Tree{
+  constructor(species, name){
+    super(species);
+    this.name = name;
+  }
+
+  static definition(){
+    return `${super(definition)} Evergreens keep their leaves all year round.`;
+  }
 }
